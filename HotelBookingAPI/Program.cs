4524d92e7f071e968ba0366054e72f8495674d26
enigmasheet <abhaymandal321@gmail.com>
2025-04-21 11:29:37 +0545

    Added The Room  Functionalility

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -30,6 +30,9 @@ namespace HotelBookingAPI
             builder.Services.AddScoped<UserRepository>();
             
             builder.Services.AddScoped<RoomTypeRepository>();
+            
+            builder.Services.AddScoped<RoomRepository>();
+
 
 
 
