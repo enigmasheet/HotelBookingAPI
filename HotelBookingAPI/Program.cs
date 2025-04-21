be51e682fc8e3e07638bf7f0fb0a10142ef5cbd5
enigmasheet <abhaymandal321@gmail.com>
2025-04-21 10:30:51 +0545

    Add project files.

---
+++ HotelBookingAPI/Program.cs
@@ -0,0 +1,42 @@
+
+using HotelBookingAPI.Connection;
+
+namespace HotelBookingAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });            
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddTransient<SqlConnectionFactory>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
